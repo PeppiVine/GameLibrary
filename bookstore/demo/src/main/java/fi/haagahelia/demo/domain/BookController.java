1a8ea10bd9fcd5bb046724908e333786446a303e
PeppiVine <peppilottaolivia@gmail.com>
2026-02-24 18:12:50 +0200

    tehty

--- bookstore/demo/src/main/java/fi/haagahelia/demo/domain/BookController.java
+++ bookstore/demo/src/main/java/fi/haagahelia/demo/domain/BookController.java
@@ -3,18 +3,54 @@ package fi.haagahelia.demo.domain;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
-private final BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
-public BookController(BookRepository bookRepository){
-    this.bookRepository=bookRepository;
-}
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
-@GetMapping("/booklist")
-public String bookList(Model model){
-    model.addAttribute("books", bookRepository.findAll());
-    return "booklist";
-}
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
+    }
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("message", "Welcome to Bookstore");
+        model.addAttribute("books", bookRepository.findAll());
+        return "index";
+    }
+
+    @GetMapping("/add")
+    public String addBookForm(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+
+    @PostMapping("/save")
+    public String saveBook(@ModelAttribute Book book) {
+        bookRepository.save(book);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteBook(@PathVariable("id") Long id) {
+        bookRepository.deleteById(id);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editBook(@PathVariable("id") Long id, Model model) {
+        Book book = bookRepository.findById(id).orElse(null);
+        model.addAttribute("book", book);
+        return "addbook";
+    }
+
 }
--- bookstoreeka/demo/src/main/java/fi/haagahelia/demo/BookController.java
+++ bookstoreeka/demo/src/main/java/fi/haagahelia/demo/BookController.java
@@ -8,9 +8,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("message", "Welcome to Bookstore");
-        return "index" ;
+        return "index";
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "booklist";
     }
 }
