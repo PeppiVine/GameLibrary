cc6517efc774c283a95ac9446f9f51a7c48c54e4
PeppiVine <peppilottaolivia@gmail.com>
2026-04-15 13:29:55 +0300

    Delete bookstore directory

--- bookstore/demo/src/main/java/fi/haagahelia/demo/domain/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.haagahelia.demo.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-
-public interface CategoryRepository extends CrudRepository<Category, Long> {
-
-    List<Category> findByName(String name);
-}
